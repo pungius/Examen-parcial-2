1905ae45db947f63986107f5acad92be7589bee6
pungius <143570409+pungius@users.noreply.github.com>
2023-12-11 22:43:27 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,106 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/info-producto/',
+    	url: 'info-producto.html',
+    	name: 'info-producto',
+  		},
+		{
+		path: '/shirts/',
+    	url: 'shirts.html',
+    	name: 'shirts',
+  		},
+		
+		{
+		path: '/sobre-nosotros/',
+    	url: 'sobre-nosotros.html',
+    	name: 'sobre-nosotros',
+  		},
+		
+	
+	],
+	dialog: {
+		title: 'Bead baazar',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#modal-login');
+		
+		app.dialog.alert('¡ Bienvenido a la APP !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnCheckout', function (e) {
+	e.preventDefault();
+
+	
+	app.dialog.confirm('¿Seguro desea finalizar su compra?', function () {
+		
+		//app.dialog.alert('Great!');
+		
+		var notification = app.notification.create({
+		  icon: '<i class="material-icons">check</i>',
+		  title: 'Order',
+		  titleRightText: '',
+		  subtitle: '',
+		  text: "Your order has been received.",
+		  closeTimeout: 3000,
+		});
+		notification.open();
+		
+	});
+	
+	
+	
+});
+
+
+
+
+
